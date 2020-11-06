c2b9bb5ec9ff4d45d4f28f8ea90d2bc08ed6cc22
Kyle <kyleedwardgjava@gmail.com>
2020-11-06 12:16:37 -0800

    updated python files

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import requests
 from flask import Flask, render_template, flash, request
 app = Flask(__name__)
 
+@app.route('/' , methods = ['GET' , 'POST'])
+def home():
+    return render_template('index.html')
+
+
 @app.route('/search' , methods = ['GET' , 'POST'])
 def search():
     return render_template('search.html')
+
+@app.route('/about' , methods = ['GET' , 'POST'])
+def about():
+    return render_template('about.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
