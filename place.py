cd33ac8c49ec3cfbfaaa16fcacceb2ec2f503974
Kyle <kyleedwardgjava@gmail.com>
2020-11-06 09:09:55 -0800

    added python files

---
+++ place.py
@@ -0,0 +1,6 @@
+class place():
+    def __init__(self, name, location, number, photo,):
+        self.name = name
+        self.location = location
+        self.number = number
+        self.photo = photo
